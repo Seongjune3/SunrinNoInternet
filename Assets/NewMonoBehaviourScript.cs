cc65829d1e3609a6739824e0742cba2bf9c81248
Seongjune3 <bini060315@gmail.com>
2026-05-16 13:09:28 +0900

    MoveFiles

---
+++ Assets/NewMonoBehaviourScript.cs
@@ -0,0 +1,27 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 5;
+    Vector2 player;
+    Vector2 a = new Vector2(1,2);
+    Vector2 b = new Vector2(2,3);
+    
+    void Start()
+    {
+        Debug.Log(Vector2.Lerp(a , b , 0.5f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player.x = Input.GetAxisRaw("Horizontal");
+        player.y = Input.GetAxisRaw("Vertical");
+
+
+        rb.AddForce(player.normalized * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Que.cs
@@ -0,0 +1,17 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Que : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 2f;
+
+    
+    void Update()
+    {
+        Vector2 direction = target.position - transform.position;
+        float angle = Mathf.Atan2(direction.y , direction.x) * Mathf.Rad2Deg;
+        Quaternion targetRotation = Quaternion.AngleAxis(angle , Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation , targetRotation , Time.deltaTime * speed); 
+    }
+}
